a7f0d99e1c99ca78dc0b68c0a32f554637a5701d
Elmir Cahangirov <elmir.cahangir@gmail.com>
2026-05-27 08:38:03 +0000

    Task is done!

---
+++ utils/redis.mjs
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+
+    this.client.connect().catch((err) => {
+      console.error('Redis Connection Error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.isOpen;
+  }
+
+  async get(key) {
+    return await this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, {
+      EX: duration
+    });
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
